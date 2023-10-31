2226fec5ff1d6e66360024bb2ca06ba83fdf51aa
RonRain <rarainero@gmail.com>
2023-10-31 16:08:42 -0400

    complete part 1 2 3

---
+++ main_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Dog
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def dog_index(request):
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dog_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+  success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
+
+
