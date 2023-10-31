b28e623d75046ce1c101ec24e3dd1447bda2e3c2
RonRain <rarainero@gmail.com>
2023-10-31 16:45:29 -0400

    add feeding in details and calendar

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 from .forms import FeedingForm
@@ -30,5 +30,13 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
   model = Dog
   success_url = '/dogs/'
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('dog-detail', dog_id=dog_id)
 
 
