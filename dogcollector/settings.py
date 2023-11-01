ed6f4716abf8bd2032169818b99bcda96d0edf89
RonRain <rarainero@gmail.com>
2023-11-01 14:51:55 -0400

    implement authorization

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = 'cat-index'
+LOGIN_URL = 'home'
+LOGIN_REDIRECT_URL = 'dog-index'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,8 @@ from .models import Dog, Toy
 from .forms import FeedingForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
   return render(request, 'home.html')
@@ -13,10 +15,12 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def dog_index(request):
-  dogs = Dog.objects.all()
+  dogs = Dog.objects.filter(user=request.user)
   return render(request, 'dogs/index.html', { 'dogs': dogs })
 
+@login_required
 def dog_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
@@ -24,21 +28,22 @@ def dog_detail(request, dog_id):
   return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form':feeding_form, 'toys': 
   toys_dog_doesnt_have })
 
-class DogCreate(CreateView):
+class DogCreate(LoginRequiredMixin, CreateView):
   model = Dog
   fields = ['name', 'breed', 'description', 'age']
   def form_valid(self, form):
     form.instance.user = self.request.user  
     return super().form_valid(form)
 
-class DogUpdate(UpdateView):
+class DogUpdate(LoginRequiredMixin, UpdateView):
   model = Dog
   fields = ['breed', 'description', 'age']
 
-class DogDelete(DeleteView):
+class DogDelete(LoginRequiredMixin, DeleteView):
   model = Dog
   success_url = '/dogs/'
 
+@login_required
 def add_feeding(request, dog_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -47,24 +52,25 @@ def add_feeding(request, dog_id):
     new_feeding.save()
   return redirect('dog-detail', dog_id=dog_id)
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys/'
 
+@login_required
 def assoc_toy(request, dog_id, toy_id):
   Dog.objects.get(id=dog_id).toys.add(toy_id)
   return redirect('dog-detail', dog_id=dog_id)
